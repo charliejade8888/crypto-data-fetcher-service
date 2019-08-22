2162e45bf915a04cadf0529def06bc03f65fc318
charliejade <charliejade@Loki-7281>
2019-08-22 09:22:03 +0100

    squash me!!

--- src/main/java/com/tyrell/replicant/crypto/data/fetcher/service/CryptoDailyDataFetcherService.java
+++ src/main/java/com/tyrell/replicant/crypto/data/fetcher/service/CryptoDailyDataFetcherService.java
@@ -34,6 +34,8 @@ public class CryptoDailyDataFetcherService implements ICryptoDataFetcherService 
 //todo have an application-test.properties too!!
     private static final Logger LOGGER = LoggerFactory.getLogger(CryptoDailyDataFetcherService.class);
 
+    //TODO put in props
+    //TODO address yellow sonar lint issues
     public static final String FYSM_PARAM_KEY = "fsym"; // from symbol
     public static final String TYSM_PARAM_KEY = "tsym"; // to symbol
     public static final String TOTS_PARAM_KEY = "toTs"; // end date in past
@@ -50,106 +52,115 @@ public class CryptoDailyDataFetcherService implements ICryptoDataFetcherService 
                 .append(env.getProperty("crypto.compare.port"))
                 .append(env.getProperty("crypto.compare.path.daily"))
                 .toString();
-        return restGetPrettifiedCryptoCompareResponse(url, cryptoDataFetcherRestParameterObject);
+        return Helper.restGetPrettifiedCryptoCompareResponse(url, cryptoDataFetcherRestParameterObject);
     }
 
-    private static String restGetPrettifiedCryptoCompareResponse(String url, CryptoDataFetcherRestParameterObject ccrpo) {
-        String originalCryptCompareResponseBody = restGet(url,ccrpo).getBody();
-        JSONArray input = new JSONArray(new JSONObject(originalCryptCompareResponseBody).get("Data").toString());
+    private static class Helper {
 
-        List<JSONObject> cryptoDataListWithHumanReadableDates = convertToListOfJsonObjectsUsingStreamsLambdas(
-                createResponseWithHumanReadableDate(input));
-        Stream<JSONObject> cryptoDataStreamWithHumanReadableDatesAndDescriptiveVolumeKeys = cryptoDataListWithHumanReadableDates
-                .parallelStream().map(CryptoDailyDataFetcherService::createResponseWithDescriptiveVolumeKeys);
+        private static String restGetPrettifiedCryptoCompareResponse(String url, CryptoDataFetcherRestParameterObject ccrpo) {
+            String originalCryptCompareResponseBody = restGet(url,ccrpo).getBody();
+            Object data = new JSONObject(originalCryptCompareResponseBody).get("Data"); //extract data section
+            JSONArray input = new JSONArray(data.toString());
 
-        JSONArray output = new JSONArray(cryptoDataStreamWithHumanReadableDatesAndDescriptiveVolumeKeys.collect(Collectors.toList()));
-        String nicelyFormattedResponseBody = output.toString(4);
+            List<JSONObject> cryptoDataListWithHumanReadableDates = convertToListOfJsonObjectsUsingStreamsLambdas(
+                    createResponseWithHumanReadableDate(input));
+            Stream<JSONObject> cryptoDataStreamWithHumanReadableDatesAndDescriptiveVolumeKeys = cryptoDataListWithHumanReadableDates
+                    .parallelStream().map(CryptoDailyDataFetcherService.Helper::createResponseWithDescriptiveVolumeKeys);
 
-        LOGGER.info("response body::" + nicelyFormattedResponseBody);
-        return nicelyFormattedResponseBody;
-    }
+            JSONArray output = new JSONArray(cryptoDataStreamWithHumanReadableDatesAndDescriptiveVolumeKeys.collect(Collectors.toList()));
+            String nicelyFormattedResponseBody = output.toString(4);
 
-    private static List<JSONObject> convertToListOfJsonObjectsUsingStreamsLambdas(JSONArray array) {
-        return arrayToStream(array).parallel()
-                .map(JSONObject.class::cast).collect(Collectors.toList());
-    }
+            LOGGER.info("response body::" + nicelyFormattedResponseBody);
+            return nicelyFormattedResponseBody;
+        }
 
-//    @Nonnull
-    private static Stream<Object> arrayToStream(JSONArray array) {
-        return StreamSupport.stream(array.spliterator(), true);
-    }
+        private static List<JSONObject> convertToListOfJsonObjectsUsingStreamsLambdas(JSONArray array) {
+            return arrayToStream(array).parallel()
+                    .map(JSONObject.class::cast).collect(Collectors.toList());
+        }
 
-    //TODO parrelelise this ::::::::::
-    private static JSONArray createResponseWithHumanReadableDate(JSONArray input) {
-        JSONArray output = new JSONArray();
-//        convertToListOfJsonObjectsUsingStreamsLambdas()
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd"); //could add hh mm ss
-        for(int i=0; i<input.length(); i++) { //parallelise??
-            JSONObject jsonObject = new JSONObject(input.get(i).toString());
+        //    @Nonnull
+        private static Stream<Object> arrayToStream(JSONArray array) {
+            return StreamSupport.stream(array.spliterator(), true);
+        }
+
+        private static JSONObject convertEpochTimeDateFieldOfJSONObjectToHumanReadableDate(JSONObject jsonObject) {
             long epochTime = Long.valueOf(jsonObject.get("time").toString());
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd"); //could add hh mm ss
             String date = sdf.format(new Date(epochTime*1000));
-            jsonObject.put("time", date);
-            output.put(jsonObject);
+            JSONObject converted = new JSONObject(jsonObject.toString());
+            converted.put("time", date);
+            return converted;
         }
-        return output;
-    }
 
-    private static JSONObject createResponseWithDescriptiveVolumeKeys(JSONObject jsonObject) {
-        JSONObject processed = new JSONObject(jsonObject.toString());
-        String volumeToKey = "volumeto";
-        String volumeFromKey = "volumefrom";
-        String quoteCurrencyVolumeInUnits = processed.get(volumeToKey).toString();
-        String baseCurrencyVolumeInUnits = processed.get(volumeFromKey).toString();
-        processed.remove(volumeToKey);
-        processed.remove(volumeFromKey);
-        processed.put("baseCurrencyVolumeInUnits", baseCurrencyVolumeInUnits);
-        processed.put("quoteCurrencyVolumeInUnits", quoteCurrencyVolumeInUnits);
-        return processed;
-    }
+        private static JSONArray createResponseWithHumanReadableDate(JSONArray input) {
+            JSONArray output;
 
-    private static ResponseEntity<String> restGet(String url, CryptoDataFetcherRestParameterObject ccrpo) {
-        RestTemplate restTemplate = new RestTemplate();
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
-                .queryParam(FYSM_PARAM_KEY, ccrpo.getBaseCurrency())
-                .queryParam(TOTS_PARAM_KEY, convertDateStringToEpoch(ccrpo.getEndDate()))
-                .queryParam(LIMIT_PARAM_KEY, getDaysBetweenStartDateAndEndDate(ccrpo))
-                .queryParam(TYSM_PARAM_KEY, ccrpo.getQuoteCurency()); //TODO if size<=1 only ret first in input
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
-        HttpEntity<?> entity = new HttpEntity<>(headers);
-        //TODO need to throw exception here if non-200!
-        return restTemplate.exchange(builder.toUriString(), GET, entity, String.class);
-    }
-
-    private static long getDaysBetweenStartDateAndEndDate(CryptoDataFetcherRestParameterObject ccrpo) {
-        SimpleDateFormat myFormat = new SimpleDateFormat("yyyy-MM-dd");
-        long daysBetween = 0L;
-        try {
-            Date dateFrom = myFormat.parse(ccrpo.getStartDate());
-            Date dateTo = myFormat.parse(ccrpo.getEndDate());
-            daysBetween = getDifferenceDays(dateFrom, dateTo);
-        } catch (ParseException e) {
-            LOGGER.error("cannot parse dates - start date: " + ccrpo.getStartDate() + " end date: " + ccrpo.getEndDate(), e); //does this print stacktrace too?
+            List<JSONObject> originalList = convertToListOfJsonObjectsUsingStreamsLambdas(input);
+            List<JSONObject> convertedList = originalList.parallelStream()
+                    .map(CryptoDailyDataFetcherService.Helper::convertEpochTimeDateFieldOfJSONObjectToHumanReadableDate).collect(Collectors.toList());
+            output = new JSONArray(convertedList);
+            return output;
         }
-        return daysBetween;
-    }
 
-    public static long getDifferenceDays(Date startDate, Date endDate) {
-        long diff = endDate.getTime() - startDate.getTime();
-        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
-    }
-
-    //TODO gradle jib
-    public static long convertDateStringToEpoch(String strDate) {
-        long epochSecond = 0L;
-        long oneDay = 1 * 24 * 60 * 60;
-        try {
-            Date d = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
-            epochSecond = d.toInstant().plusSeconds(oneDay).atOffset(ZoneOffset.UTC).toEpochSecond();
-        } catch (ParseException e) {
-            e.printStackTrace();
+        private static JSONObject createResponseWithDescriptiveVolumeKeys(JSONObject jsonObject) {
+            JSONObject processed = new JSONObject(jsonObject.toString());
+            String volumeToKey = "volumeto";
+            String volumeFromKey = "volumefrom";
+            String quoteCurrencyVolumeInUnits = processed.get(volumeToKey).toString();
+            String baseCurrencyVolumeInUnits = processed.get(volumeFromKey).toString();
+            processed.remove(volumeToKey);
+            processed.remove(volumeFromKey);
+            processed.put("baseCurrencyVolumeInUnits", baseCurrencyVolumeInUnits);
+            processed.put("quoteCurrencyVolumeInUnits", quoteCurrencyVolumeInUnits);
+            return processed;
         }
-        return epochSecond;
+
+        private static ResponseEntity<String> restGet(String url, CryptoDataFetcherRestParameterObject ccrpo) {
+            RestTemplate restTemplate = new RestTemplate();
+            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+                    .queryParam(FYSM_PARAM_KEY, ccrpo.getBaseCurrency())
+                    .queryParam(TOTS_PARAM_KEY, convertDateStringToEpoch(ccrpo.getEndDate()))
+                    .queryParam(LIMIT_PARAM_KEY, getDaysBetweenStartDateAndEndDate(ccrpo))
+                    .queryParam(TYSM_PARAM_KEY, ccrpo.getQuoteCurency()); //TODO if size<=1 only ret first in input
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+            HttpEntity<?> entity = new HttpEntity<>(headers);
+            //TODO need to throw exception here if non-200!
+            return restTemplate.exchange(builder.toUriString(), GET, entity, String.class);
+        }
+
+        private static long getDaysBetweenStartDateAndEndDate(CryptoDataFetcherRestParameterObject ccrpo) {
+            SimpleDateFormat myFormat = new SimpleDateFormat("yyyy-MM-dd");
+            long daysBetween = 0L;
+            try {
+                Date dateFrom = myFormat.parse(ccrpo.getStartDate());
+                Date dateTo = myFormat.parse(ccrpo.getEndDate());
+                daysBetween = getDifferenceDays(dateFrom, dateTo);
+            } catch (ParseException e) {
+                LOGGER.error("cannot parse dates - start date: " + ccrpo.getStartDate() + " end date: " + ccrpo.getEndDate(), e); //does this print stacktrace too?
+            }
+            return daysBetween;
+        }
+
+        private static long getDifferenceDays(Date startDate, Date endDate) {
+            long diff = endDate.getTime() - startDate.getTime();
+            return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+        }
+
+        //TODO gradle jib
+        private static long convertDateStringToEpoch(String strDate) {
+            long epochSecond = 0L;
+            long oneDay = 1 * 24 * 60 * 60;
+            try {
+                Date d = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+                epochSecond = d.toInstant().plusSeconds(oneDay).atOffset(ZoneOffset.UTC).toEpochSecond();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return epochSecond;
+        }
+
     }
 
 }
